41212d0847da6e9e49fd22e7039e16a8d7516cf3
Hever Farber <hever.farber@gmail.com>
2021-12-21 14:34:20 +0200

    fix-vars

--- aws/variables.tf
+++ aws/variables.tf
@@ -22,25 +22,25 @@ variable "min_capacity" {
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
 
-  default = null
+  default = "172.16.0.0/16"
 }
 
 variable "private_subnets" {
   description = "List of private subnets inside the VPC"
 
-  default = null
+  default = ["172.16.0.0/21", "172.16.16.0/21", "172.16.32.0/21", "172.16.48.0/21", "172.16.64.0/21"]
 }
 
 variable "public_subnets" {
   description = "List of public subnets inside the VPC"
 
-  default = null
+  default = ["172.16.8.0/22", "172.16.24.0/22", "172.16.40.0/22", "172.16.56.0/22", "172.16.72.0/22"]
 }
 
 variable "instance_type" {
-  default = null
+  default = "t3a.2xlarge" # 8vCPUs 32GB
 }
 
 variable "reclaim_policy" {
-  default = null
+  default = "Retain"
 }
