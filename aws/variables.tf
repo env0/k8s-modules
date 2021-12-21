f3dfbf01976b002f78dcee9e72f07feace51398f
Hever Farber <hever.farber@gmail.com>
2021-12-21 14:12:37 +0200

    default

--- aws/variables.tf
+++ aws/variables.tf
@@ -21,16 +21,26 @@ variable "min_capacity" {
 
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+
+  default = null
 }
 
 variable "private_subnets" {
   description = "List of private subnets inside the VPC"
+
+  default = null
 }
 
 variable "public_subnets" {
   description = "List of public subnets inside the VPC"
+
+  default = null
 }
 
-variable "instance_type" {}
+variable "instance_type" {
+  default = null
+}
 
-variable "reclaim_policy" {}
+variable "reclaim_policy" {
+  default = null
+}
