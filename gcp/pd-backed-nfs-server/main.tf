1dd4af5148734f13ff9264dd85e783fa3477a316
avner.sorek <avner.sorek@env0.com>
2022-01-23 19:11:40 +0200

    Wip

---
+++ gcp/pd-backed-nfs-server/main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_disk" "env0_internal_state_disk" {
+  name  = "env0_internal_state_disk"
+  type  = "pd-ssd"
+  size = "300Gi"
+}
+
+data "template_file" "nfs_server_k8s_yaml" {
+  template = file("./nfs-server.yaml")
+  vars = {
+    pdName = google_compute_disk.env0_internal_state_disk.name
+    pdZone = google_compute_disk.env0_internal_state_disk.zone
+  }
+}
+
+data "kubectl_file_documents" "nfs_server_k8s_docs" {
+  content = data.template_file.rendered
+}
+
+resource "kubectl_manifest" "nfs_server_k8s_manifest" {
+  for_each  = data.nfs_server_k8s_docs.docs.manifests
+  yaml_body = each.value
+}
---
+++ gcp/pd-backed-nfs-server/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    google = "4.7.0"
+    template = "2.2.0"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.13.1"
+    }
+  }
+}
