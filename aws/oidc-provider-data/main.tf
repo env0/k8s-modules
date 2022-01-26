9d820f1efa72683d80e3dcc98f8e7bcacdbe26df
Liran Farage <liran.farage@env0.com>
2022-01-26 20:39:10 +0200

    wip

--- aws/oidc-provider-data/main.tf
+++ aws/oidc-provider-data/main.tf
@@ -9,6 +9,6 @@ locals {
   issuer_url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
   partition    = data.aws_partition.current.id
   account_id   = data.aws_caller_identity.current.account_id
-  url_stripped = replace(issuer_url, "https://", "")
+  url_stripped = replace(local.issuer_url, "https://", "")
   arn          = "arn:${local.partition}:iam::${local.account_id}:oidc-provider/${local.url_stripped}"
 }
