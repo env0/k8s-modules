e10391eeb487702c85c40d9ebf1a098373e94079
Yaron Yarimi <yaron.yarimi@env0.com>
2022-01-26 21:05:18 +0200

    replace/remove kubectl provider version in favor of kubernetes provider version

--- aws/autoscaler/versions.tf
+++ aws/autoscaler/versions.tf
@@ -4,9 +4,6 @@ terraform {
   required_providers {
     aws        = "~> 3.68.0"
     helm       = "~> 2.4.0"
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
+    kubernetes = "~> 2.7.0"
   }
 }
