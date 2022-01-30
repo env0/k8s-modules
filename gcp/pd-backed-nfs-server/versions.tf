37902401f34450d287420cd09a6881c39932275b
Yaron Yarimi <yaron.yarimi@env0.com>
2022-01-30 18:10:44 +0200

    re-add kubectl provider to allow graceful transition to kubernetes provider

--- gcp/pd-backed-nfs-server/versions.tf
+++ gcp/pd-backed-nfs-server/versions.tf
@@ -3,6 +3,10 @@ terraform {
 
   required_providers {
     google   = "~> 4.7.0"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.13.1"
+    }
     kubernetes = "~> 2.7.0"
   }
 }
