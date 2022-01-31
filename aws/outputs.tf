a57094e8ddb091d7cd4ddceee82b4ba8fd5830c5
eladmoshe <elad.moshe@env0.com>
2022-02-01 00:09:22 +0200

    fix

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,9 +1,9 @@
 output "kubernetes_host" {
-  value       = module.eks.create ? module.eks[0].kubernetes_host : var.eks.kubernetes_host
+  value       = var.eks.create ? module.eks[0].kubernetes_host : var.eks.kubernetes_host
   description = "EKS cluster host endpoint"
 }
 
 output "cluster_id" {
-  value       = module.eks.create ? module.eks[0].cluster_id : var.eks.cluster_id
+  value       = var.eks.create ? module.eks[0].cluster_id : var.eks.cluster_id
   description = "EKS cluster id"
 }
