108b3b2f675e4542cedeec4ab3ac77aa26cb614c
eladmoshe <elad.moshe@env0.com>
2022-02-06 15:35:55 +0200

    refactoring main to use new modules_info

--- aws/main.tf
+++ aws/main.tf
@@ -1,16 +1,16 @@
 data aws_eks_cluster "my_eks" {
-  count = var.eks.create ? 0 : 1
-  name = var.eks.cluster_id
+  count = var.modules_info.eks.create ? 0 : 1
+  name = var.modules_info.eks.cluster_id
 }
 
 locals {
-  vpc_id = (var.efs.create && var.eks.create && var.vpc.create) ? module.vpc[0].vpc_id : var.vpc.vpc_id
-  private_subnets = (var.efs.create && var.eks.create && var.vpc.create) ? module.vpc[0].private_subnets : var.vpc.private_subnets
-  efs_id = var.efs.create ? module.efs[0].efs_id : var.efs.efs_id
+  vpc_id = (var.modules_info.efs.create && var.modules_info.eks.create && var.modules_info.vpc.create) ? module.vpc[0].vpc_id : var.modules_info.vpc.id
+  private_subnets = (var.modules_info.efs.create && var.modules_info.eks.create && var.modules_info.vpc.create) ? module.vpc[0].private_subnets : var.modules_info.vpc.private_subnets
+  efs_id = var.modules_info.efs.create ? module.efs[0].efs_id : var.modules_info.efs.id
 }
 
 module "vpc" {
-  count           = var.vpc.create ? 1 : 0
+  count           = var.modules_info.vpc.create ? 1 : 0
   source          = "./vpc"
 
   cluster_name    = var.cluster_name
@@ -21,7 +21,7 @@ module "vpc" {
 
 module "eks" {
   depends_on = [module.vpc]
-  count         = var.eks.create ? 1 : 0
+  count         = var.modules_info.eks.create ? 1 : 0
   source     = "./eks"
 
   vpc_id        = local.vpc_id
@@ -32,7 +32,7 @@ module "eks" {
 }
 
 module "efs" {
-  count        = var.efs.create ? 1 : 0
+  count        = var.modules_info.efs.create ? 1 : 0
   depends_on   = [module.eks, module.vpc]
   source       = "./efs"
 
@@ -43,7 +43,7 @@ module "efs" {
 }
 
 module "autoscaler" {
-  count      = var.create_autoscaler ? 1 : 0
+  count      = var.modules_info.autoscaler.create ? 1 : 0
   depends_on = [module.eks]
   source     = "./autoscaler"
 
@@ -51,7 +51,7 @@ module "autoscaler" {
 }
 
 module "csi_driver" {
-  count      = var.create_csi_driver ? 1 : 0
+  count      = var.modules_info.csi_driver.create ? 1 : 0
   depends_on = [module.efs]
   source     = "./csi-driver"
 
