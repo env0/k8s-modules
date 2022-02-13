4b852fbb7433773f0e8540d3c6b43c87400d6ed5
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-02-13 16:56:05 +0200

    Use NFS Ganesha NFS Server and Provisioner (#14)

--- gcp/main.tf
+++ gcp/main.tf
@@ -15,7 +15,24 @@ provider "kubernetes" {
   token            = data.google_client_config.provider.access_token
 }
 
-module "pd_backed_nfs_server" {
-  source     = "./pd-backed-nfs-server"
+provider "helm" {
+  kubernetes {
+    host = "https://${data.google_container_cluster.my_cluster.endpoint}"
+    cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+    )
+    token            = data.google_client_config.provider.access_token
+  }
+}
+
+resource "helm_release" "nfs_server_provisioner" {
+  name       = "nfs-server-provisioner"
+  repository = "https://kubernetes-sigs.github.io/nfs-ganesha-server-and-external-provisioner/"
+  chart      = "nfs-server-provisioner"
+  timeout    = 600
+
+  values = [
+    "${file("${path.module}/values.yaml")}"
+  ]
 }
 
--- gcp/pd-backed-nfs-server/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    google   = "~> 4.7.0"
-    kubernetes = "~> 2.7.0"
-  }
-}
