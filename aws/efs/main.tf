107d7ed0e44d0878c308aac145d3feb3f5dd5e1a
Andrew Way <andrew.way@env0.com>
2022-10-11 13:20:45 -0700

    add readme and update efs version

--- aws/efs/main.tf
+++ aws/efs/main.tf
@@ -6,31 +6,21 @@ module "efs" {
   depends_on = [data.aws_eks_cluster.cluster]
 
   source  = "cloudposse/efs/aws"
-  version = "0.31.1"
+  version = "0.32.7"
 
   region = var.region
 
   vpc_id  = var.vpc_id
   subnets = var.subnets
 
-  transition_to_ia    = "AFTER_7_DAYS"
-
+  transition_to_ia    = ["AFTER_30_DAYS"]
+  
+  allowed_security_group_ids = [data.aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id]
+  
   // NOTE: the module is stupid and puts this tag on the security group and access point as well
   tags = {
     Name = "${var.cluster_name}-state-efs"
   }
-
-  security_group_rules = [
-    {
-      type                     = "ingress"
-      from_port                = 2049
-      to_port                  = 2049
-      protocol                 = "tcp"
-      cidr_blocks              = []
-      source_security_group_id = data.aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
-      description              = "Allow ingress traffic to EFS from primary EKS security group"
-    }
-  ]
 }
 
 resource "aws_efs_backup_policy" "policy" {
