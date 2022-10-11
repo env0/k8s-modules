a07698988c824235f1d1f6bf6e595f03dcfebf6e
Andrew Way <andrew.way@env0.com>
2022-10-11 16:25:25 -0700

    change version dependencies for easier testing

--- aws/csi-driver/versions.tf
+++ aws/csi-driver/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws        = "~> 3.68.0"
-    kubernetes = "=> 2.9.0"
-    helm       = "~> 2.7.0"
+    aws        = ">= 3.68.0"
+    kubernetes = ">= 2.9.0"
+    helm       = ">= 2.7.0"
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
