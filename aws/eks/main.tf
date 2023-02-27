ab92d955b3d6d84b25976358dc46ec97b17bcb30
Liran Farage <liran.farage@env0.com>
2023-02-27 19:04:13 +0200

    Fix: Add k8s provider data

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -6,6 +6,20 @@ data "aws_subnet_ids" "private" {
   }
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
