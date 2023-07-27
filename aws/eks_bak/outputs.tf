130793a29e0e35c80ddc20d325e4babb18386233
Liran Farage <liran.farage@env0.com>
2023-07-27 12:37:36 +0300

    removed backup

--- aws/eks_bak/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kubernetes_host" {
-  value       = module.eks.cluster_endpoint
-  description = "EKS cluster host endpoint"
-}
-
-output "cluster_id" {
-  value       = module.eks.cluster_id
-  description = "EKS cluster id"
-}
--- aws/eks_bak/versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws        = "~> 3.68.0"
-  }
-}
