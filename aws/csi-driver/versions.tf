dcc3441385d8745110f5c681f874a76de3093a35
Tomer Landesman <103362486+tomer-landesman@users.noreply.github.com>
2023-10-24 10:54:07 +0300

    Update versions.tf

--- aws/csi-driver/versions.tf
+++ aws/csi-driver/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws        = ">= 4.0.0"
-    kubernetes = "~> 2.11.0"
+    kubernetes = ">= 2.11.0"
     helm       = "~> 2.10.1"
     kubectl = {
       source  = "gavinbunney/kubectl"
