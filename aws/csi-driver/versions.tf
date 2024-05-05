c27ba2c893a67370dc46e4270a99e121cece0162
ChaimP <chaim.platonov@env0.com>
2024-05-05 13:44:09 +0300

    add required providers versions

--- aws/csi-driver/versions.tf
+++ aws/csi-driver/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws  = ">= 5.40.0"
-    helm = ">= 2.13.0"
+    aws        = ">= 5.40.0"
+    helm       = ">= 2.13.0"
+    kubernetes = ">= 2.29.0"
   }
 }
