8df5b285f4e74a7418e2d9ef98e57902aae2cd0c
ChaimP <chaim.platonov@env0.com>
2024-05-05 13:37:55 +0300

    move section

--- aws/autoscaler/main.tf
+++ aws/autoscaler/main.tf
@@ -7,18 +7,6 @@ locals {
   autoscaling_group_name = data.aws_eks_node_group.node_group.resources.0.autoscaling_groups.0.name
 }
 
-# https://github.com/kubernetes/autoscaler/tree/master/cluster-autoscaler/cloudprovider/aws#common-notes-and-gotchas
-resource "null_resource" "autoscaling_settings" {
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<-SCRIPT
-      aws autoscaling suspend-processes --auto-scaling-group-name ${local.autoscaling_group_name} --scaling-processes AZRebalance
-      aws autoscaling update-auto-scaling-group --auto-scaling-group-name ${local.autoscaling_group_name} --default-cooldown 60
-      aws autoscaling enable-metrics-collection --auto-scaling-group-name ${local.autoscaling_group_name} --granularity "1Minute"
-    SCRIPT
-  }
-}
-
 module "eks-cluster-autoscaler" {
   source  = "lablabs/eks-cluster-autoscaler/aws"
   version = "2.2.0"
@@ -37,4 +25,16 @@ module "eks-cluster-autoscaler" {
     #  scale-down-utilization-threshold: local.single_deployment_pod_utilization_of_node_resources
     #}
   })
+}
+
+# https://github.com/kubernetes/autoscaler/tree/master/cluster-autoscaler/cloudprovider/aws#common-notes-and-gotchas
+resource "null_resource" "autoscaling_settings" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-SCRIPT
+      aws autoscaling suspend-processes --auto-scaling-group-name ${local.autoscaling_group_name} --scaling-processes AZRebalance
+      aws autoscaling update-auto-scaling-group --auto-scaling-group-name ${local.autoscaling_group_name} --default-cooldown 60
+      aws autoscaling enable-metrics-collection --auto-scaling-group-name ${local.autoscaling_group_name} --granularity "1Minute"
+    SCRIPT
+  }
 }
