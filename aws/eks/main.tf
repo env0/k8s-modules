22223494988328cd33ece5361566af05c3f3b2b9
Yaron Yarimi <yaron.yarimi@env0.com>
2025-02-23 10:58:26 +0200

    revert

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -47,7 +47,7 @@ module "eks" {
       use_name_prefix = false
 
       min_size     = var.min_capacity
-      desired_size = 2
+      desired_size = var.min_capacity
       max_size     = var.max_capacity
 
       update_config = {
