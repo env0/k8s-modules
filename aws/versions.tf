ebf7b5a58ccdaee3524601bfbd5b11d6e89f26e3
Chaim Platonov <chaim.platonov@env0.com>
2025-06-24 11:13:38 +0300

    add providers version constraints

--- aws/versions.tf
+++ aws/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws        = ">= 5.40.0"
-    helm       = ">= 2.13.0"
+    aws        = ">= 5.40.0, < 6.0.0"
+    helm       = ">= 2.13.0, < 3.0.0"
     kubernetes = ">= 2.29.0"
   }
 }
