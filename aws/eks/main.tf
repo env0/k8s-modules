cc649de9986a18788a756ac47b86d0d08d252a76
Hever Farber <hever.farber@gmail.com>
2025-11-10 14:17:11 +0200

    Update EKS addons to remove version specifications
    Removed specific addon versions for kube-proxy and vpc-cni.

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -62,19 +62,14 @@ module "eks" {
   cluster_addons = {
     coredns = {
       preserve = true
-      addon_version = "v1.12.4-eksbuild.1"
 
       timeouts = {
         create = "25m"
         delete = "10m"
       }
     }
-    kube-proxy = {
-      addon_version = "v1.34.0-eksbuild.4"
-    }
-    vpc-cni    = {
-      addon_version = "v1.20.4-eksbuild.2"
-    }
+    kube-proxy = {}
+    vpc-cni    = {}
   }
 
   create_kms_key            = false
